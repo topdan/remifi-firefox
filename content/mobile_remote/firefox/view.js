bb887c29fb2aa890a3d0510ac6c1d91acafb2ee2
Dan Cunning <dancunning@gmail.com>
2012-04-17 21:32:00 -0400

    button added to the toolbar after the home button by default

--- content/mobile_remote/firefox/view.js
+++ content/mobile_remote/firefox/view.js
@@ -23,4 +23,31 @@ MobileRemote.Firefox.View = function() {
     }
   }
   
+  this.install = function(toolbarId, id, afterId) {
+    if (!document.getElementById(id)) {
+      var toolbar = document.getElementById(toolbarId);
+      
+      // If no afterId is given, then append the item to the toolbar
+      var before = null;
+      if (afterId) {
+        var elem = document.getElementById(afterId);
+        if (elem && elem.parentNode == toolbar)
+          before = elem.nextElementSibling;
+      }
+      
+      toolbar.insertItem(id, before);
+      toolbar.setAttribute("currentset", toolbar.currentSet);
+      document.persist(toolbar.id, "currentset");
+      
+      if (toolbarId == "addon-bar")
+        toolbar.collapsed = false;
+    }
+  }
+  
+  var firstRunPref = "extensions.mobile-remote.firstRunDone";
+  if (!Application.prefs.getValue(firstRunPref, false)) {
+    Application.prefs.setValue(firstRunPref, true);
+    this.install("nav-bar", "mobile-remote-button", "home-button");
+  }
+  
 }
