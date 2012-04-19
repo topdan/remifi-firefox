4945cbea4315f4a008d305dd75b04221fae2ce99
Dan Cunning <dancunning@gmail.com>
2012-04-19 19:03:29 -0400

    separate coffee and js files

--- content/mobile_remote/utils/microtemplate.js
+++
@@ -1,7 +0,0 @@
-(function() {
-
-  MobileRemote.microtemplate = function(code) {
-    return new Function("obj", "var p=[],print=function(){p.push.apply(p,arguments);};" + "with(obj){p.push('" + code.replace(/[\r\t\n]/g, " ").split("<%").join("\t").replace(/((^|%>)[^\t]*)'/g, "$1\r").replace(/\t=(.*?)%>/g, "',$1,'").split("\t").join("');").split("%>").join("p.push('").split("\r").join("\\'") + "');}return p.join('');");
-  };
-
-}).call(this);
